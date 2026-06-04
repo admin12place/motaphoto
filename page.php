dc54f0cb4316d9dea43f664d182adee23e780187
admin12place <thierry@randovelo34.fr>
2026-06-04 17:44:30 +0200

    initial étape 3

--- page.php
+++ page.php
@@ -7,20 +7,29 @@
 	$image_url = wp_get_attachment_image_url($image_id, 'full');
 	
 ?>
-
+<?php
+	if (is_front_page() ) :
+?>
 <div class="header-image" style="background-image: url('<?php echo esc_url($image_url); ?>');"><!--L'image sur le background-->
 	<h1 class="header-title">PHOTOGRAPHE EVENT</h1>
 </div>
 <?php
-/* Start the Loop */
-while ( have_posts() ) :
+	endif;
+?>
+
+<?php
+
+the_content();
+
+/* Start the Loop*/
+/*while ( have_posts() ) :
 	the_post();
 	get_template_part( 'template-parts/content/content-page' );
-	the_content();
+	
 	// If comments are open or there is at least one comment, load up the comment template.
 	if ( comments_open() || get_comments_number() ) {
 		comments_template();
 	}
-endwhile; // End of the loop.
+endwhile; // End of the loop.*/
 
 get_footer();
