ab6bc098eaf6fbac24769255398f45ffd6fe29fc
admin12place <thierry@randovelo34.fr>
2026-06-05 18:46:21 +0200

    mise en place du single page (debut)

--- page.php
+++ page.php
@@ -8,28 +8,20 @@
 	
 ?>
 <?php
-	if (is_front_page() ) :
-?>
-<div class="header-image" style="background-image: url('<?php echo esc_url($image_url); ?>');"><!--L'image sur le background-->
-	<h1 class="header-title">PHOTOGRAPHE EVENT</h1>
-</div>
-<?php
-	endif;
-?>
+	while ( have_posts() ) :
+    	the_post();
+		if (is_front_page() ) :
+			?>
+			<div class="header-image" style="background-image: url('<?php echo esc_url($image_url); ?>');"><!--L'image sur le background-->
+				<h1 class="header-title">PHOTOGRAPHE EVENT</h1>
+			</div>
+			<?php
+				endif;
+			?>
 
-<?php
+			<?php
 
-the_content();
-
-/* Start the Loop*/
-/*while ( have_posts() ) :
-	the_post();
-	get_template_part( 'template-parts/content/content-page' );
-	
-	// If comments are open or there is at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
-endwhile; // End of the loop.*/
+		the_content();
+	endwhile;
 
 get_footer();
---
+++ single-photo.php
@@ -0,0 +1,56 @@
+<?php
+
+    get_header();
+
+    while ( have_posts() ) : the_post() ;
+
+    $photo_file = mb_strtoupper ('Fichier : ' . esc_html(SCF::get('photo_file')), 'UTF-8');
+    $photo_file_url = wp_get_attachment_url(SCF::get('photo_file'));
+    $photo_title = mb_strtoupper (esc_html(SCF::get('photo_title')), 'UTF-8');
+    $photo_reference =  mb_strtoupper ('référence : ' . esc_html(SCF::get('photo_reference')), 'UTF-8');
+    $photo_year =   mb_strtoupper ('année : ' . esc_html(SCF::get('photo_year')), 'UTF-8');
+    $photo_type =  mb_strtoupper ('type : ' . esc_html(SCF::get('photo_type')), 'UTF-8');
+    $class_format = strtolower (esc_html(SCF::get('photo_format')));
+    $photo_format =  mb_strtoupper ('format : ' . esc_html(SCF::get('photo_format')), 'UTF-8');
+
+    $photo_categorie = [];
+        $cats = get_the_terms(get_the_ID(), 'categorie');
+        if ($cats && !is_wp_error($cats)) {
+            foreach ($cats as $cat) { $photo_categorie[] = $cat->name; } }
+    $photo_categorie =  mb_strtoupper ('catégorie : ' . implode (', ', $photo_categorie), 'UTF-8');
+?>
+
+<!--STRUCTURE HTML DE LA PAGE SINGLE-->
+<article id="main-single">
+    <div class="post-photo-single">
+        <div class="desc-photo-single <?php echo $class_format; ?>">
+            <h2><?php echo $photo_title; ?></h2>
+            <span class="desc-item-photo"><?php echo $photo_reference; ?></span>
+            <span class="desc-item-photo"><?php echo $photo_categorie; ?></span>
+            <span class="desc-item-photo"><?php echo $photo_format; ?></span>
+            <span class="desc-item-photo"><?php echo $photo_type; ?></span>
+            <span class="desc-item-photo"><?php echo $photo_year; ?></span>
+        </div>
+        <div class="img-photo-single">
+        <img class="" src="<?php echo esc_url($photo_file_url);?>"/>
+        </div>
+
+    </div>
+    <div class="link-slider-single">
+        
+    </div>
+    <div class="like-photo-single">
+    </div>
+
+</article>
+
+
+    
+<?php
+endwhile;
+
+get_footer();
+
+//the_title('<h1>', '</h1>');
+//the_post_thumbnail('large');
+//the_content();
